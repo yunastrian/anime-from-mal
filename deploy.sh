d2bd70167a5e78ab7bcf412ec9fa6230b20fcd43
Kurniandha Sukma Yunastrian <ykurniandha@gmail.com>
2023-02-18 21:34:35 +0700

    Add deployment script

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+# navigate into the build output directory
+cd dist
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B deploy
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/yunastrian/anime-from-mal deploy:gh-pages
+
+cd -
