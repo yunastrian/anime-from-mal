e288fdbca5a9d19c0d2fb2d6c85cc04dde47c822
Kurniandha Sukma Yunastrian <ykurniandha@gmail.com>
2023-02-18 19:34:06 +0700

    Add data and pagination

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <script>
-import animes from './data/file2.json'
+import animes from './data/animes.json'
 export default {
   data() {
     return {
-      items: animes.data
+      allItems: animes.data,
+      filteredItems: animes.data,
+      items: animes.data.slice(0, 10),
+      page: 1,
+      maxPages: Math.ceil(animes.data.length/10)
     };
   },
+  methods: {
+    changePage(newPage) {
+      this.page = newPage
+      this.items = this.filteredItems.slice((newPage-1)*10, newPage*10)
+    }
+  }
 };
 </script>
 
@@ -13,21 +23,21 @@ export default {
   <div class="container">
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <a class="navbar-brand" href="#">Anime from MAL</a>
       </div>
     </nav>
     <div class="row">
       <div
         v-for="item in items"
         :key="item.id"
-        class="card mb-3"
+        class="card mb-4"
         style="max-width: 540px"
       >
         <div class="row g-0">
           <div class="col-md-4">
             <img
               :src=item[3]
-              class="img-fluid rounded-start"
+              class="img-fluid rounded-start img-responsive"
               alt="..."
             />
           </div>
@@ -43,7 +53,25 @@ export default {
         </div>
       </div>
     </div>
+    <nav aria-label="Page navigation example">
+      <ul class="pagination justify-content-center">
+        <li v-if="this.page > 1"  @click="changePage(1)" class="page-item"><a class="page-link fw-semibold" href="#">First</a></li>
+        <li v-if="this.page > 1"  @click="changePage(this.page - 1)" class="page-item"><a class="page-link" href="#">Previous</a></li>
+        <li v-if="this.page > 2" class="page-item"><a class="page-link">...</a></li>
+        <li v-if="this.page > 1"  @click="changePage(this.page - 1)" class="page-item"><a class="page-link" href="#">{{ page - 1 }}</a></li>
+        <li class="page-item"><a class="page-link fw-bold">{{ page }}</a></li>
+        <li v-if="this.page < maxPages"  @click="changePage(this.page + 1)" class="page-item"><a class="page-link" href="#">{{ page + 1 }}</a></li>
+        <li v-if="this.page < maxPages - 1" class="page-item"><a class="page-link">...</a></li>
+        <li v-if="this.page < maxPages" @click="changePage(this.page + 1)" class="page-item"><a class="page-link" href="#">Next</a></li>
+        <li v-if="this.page < maxPages"  @click="changePage(maxPages)" class="page-item"><a class="page-link fw-semibold" href="#">Last</a></li>
+      </ul>
+    </nav>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.img-responsive {
+  width: 100%;
+  height: auto;
+}
+</style>
